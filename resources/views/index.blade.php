b3034482415b4223756c24a5415be951bdea3fa1
Dušan Plavák <dusan.plavak@gmail.com>
2016-08-18 09:17:12 +0200

    Change css cdn.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         {!! Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css') !!}
         {!! Html::style('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css') !!}
         {!! Html::style('https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css') !!}
-        {!! Html::style('http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css') !!}
+        {!! Html::style('https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/leaflet.css') !!}
         {!! Html::style('adminlte/css/AdminLTE.css') !!}
         {!! Html::style('adminlte/css/skins/_all-skins.css') !!}
         {!! Html::style('css/custom.css') !!}
