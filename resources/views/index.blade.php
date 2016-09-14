8b48c6848d0733e1b16c509e509cf5e8c3823c5e
Dušan Plavák <dusan.plavak@gmail.com>
2016-09-14 12:09:20 +0200

    Fixnuty title stranky.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Nextis</title>
+        <title>Nexteria Space</title>
         {!! Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css') !!}
         {!! Html::style('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css') !!}
         {!! Html::style('https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css') !!}
