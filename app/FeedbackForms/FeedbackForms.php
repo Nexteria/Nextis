4fa28c78a011bd28bb49192c71eed75cb96705b4
Dušan Plavák <dusan.plavak@gmail.com>
2017-05-13 13:19:20 +0200

    Fixnute overovanie feedback linku.

--- app/FeedbackForms/FeedbackForms.php
+++ app/FeedbackForms/FeedbackForms.php
@@ -12,71 +12,73 @@ class FeedbackForms
      * @param  string  $formUrl
      * @return string
      */
-    public static function validate($formUrl)
+    public static function validate($formUrl, $maxRetries = 15)
     {
-        $client = new \Google_Client();
-        $client->setApplicationName(env('GOOGLE_APPLICATION_NAME', ''));
-        $client->setScopes(implode(' ', ['https://www.googleapis.com/auth/forms']));
-        $client->setAuthConfig(env('GOOGLE_SECRET_JSON_LOCATION', ''));
-        $client->setAccessType('offline');
+        while ($maxRetries > 0) {
+            $client = new \Google_Client();
+            $client->setApplicationName(env('GOOGLE_APPLICATION_NAME', ''));
+            $client->setScopes(implode(' ', ['https://www.googleapis.com/auth/forms']));
+            $client->setAuthConfig(env('GOOGLE_SECRET_JSON_LOCATION', ''));
+            $client->setAccessType('offline');
 
-        // Load previously authorized credentials from a file.
-        $credentialsPath = env('GOOGLE_AUTH_TOKEN_JSON_LOCATION', '');
-        $accessToken = json_decode(file_get_contents($credentialsPath), true);
-        $client->setAccessToken($accessToken);
+            // Load previously authorized credentials from a file.
+            $credentialsPath = env('GOOGLE_AUTH_TOKEN_JSON_LOCATION', '');
+            $accessToken = json_decode(file_get_contents($credentialsPath), true);
+            $client->setAccessToken($accessToken);
 
-        // Refresh the token if it's expired.
-        if ($client->isAccessTokenExpired()) {
-            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-            file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
-        }
-
-        $request = new \Google_Service_Script_ExecutionRequest();
-        $request->setFunction('validateForm');
-        $request->setParameters($formUrl);
-        
-        $service = new \Google_Service_Script($client);
-        try {
-            // Make the API request.
-            $response = $service->scripts->run(env('GOOGLE_SCRIPT_ID', ''), $request);
-
-            if ($response->getError()) {
-                // The API executed, but the script returned an error.
-
-                // Extract the first (and only) set of error details. The values of this
-                // object are the script's 'errorMessage' and 'errorType', and an array of
-                // stack trace elements.
-                $error = $response->getError()['details'][0];
-                \Log::error("Script error message: ".$error['errorMessage']);
-
-                if (array_key_exists('scriptStackTraceElements', $error)) {
-                    // There may not be a stacktrace if the script didn't start executing.
-                    \Log::error("Script error stacktrace:\n");
-                    foreach ($error['scriptStackTraceElements'] as $trace) {
-                        \Log::error("\t".$trace['function']." ".$trace['lineNumber']);
-                    }
-                }
-
-                return [
-                    'code' => 500,
-                    'error' => 'Fatal error! Please contact NxSpace Admin!',
-                ];
-            } else {
-                // The structure of the result will depend upon what the Apps Script
-                // function returns. Here, the function returns an Apps Script Object
-                // with String keys and values, and so the result is treated as a
-                // PHP array (folderSet).
-                $resp = $response->getResponse();
-                return json_decode($resp['result'], true);
+            // Refresh the token if it's expired.
+            if ($client->isAccessTokenExpired()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+                file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
             }
-        } catch (\Exception $e) {
-            // The API encountered a problem before the script started executing.
-            \Log::error('Caught exception: '.$e->getMessage()."\n");
-            return [
-                'code' => 500,
-                'error' => 'Fatal error! Please contact NxSpace Admin!',
-            ];
+
+            $request = new \Google_Service_Script_ExecutionRequest();
+            $request->setFunction('validateForm');
+            $request->setParameters($formUrl);
+            
+            $service = new \Google_Service_Script($client);
+            try {
+                // Make the API request.
+                $response = $service->scripts->run(env('GOOGLE_SCRIPT_ID', ''), $request);
+
+                if ($response->getError()) {
+                    // The API executed, but the script returned an error.
+
+                    // Extract the first (and only) set of error details. The values of this
+                    // object are the script's 'errorMessage' and 'errorType', and an array of
+                    // stack trace elements.
+                    $error = $response->getError()['details'][0];
+                    \Log::error("Script error message: ".$error['errorMessage']);
+
+                    if (array_key_exists('scriptStackTraceElements', $error)) {
+                        // There may not be a stacktrace if the script didn't start executing.
+                        \Log::error("Script error stacktrace:\n");
+                        foreach ($error['scriptStackTraceElements'] as $trace) {
+                            \Log::error("\t".$trace['function']." ".$trace['lineNumber']);
+                        }
+                    }
+
+                    throw new \Exception("Error during validating feedback form", 1);
+                } else {
+                    // The structure of the result will depend upon what the Apps Script
+                    // function returns. Here, the function returns an Apps Script Object
+                    // with String keys and values, and so the result is treated as a
+                    // PHP array (folderSet).
+                    $resp = $response->getResponse();
+                    return json_decode($resp['result'], true);
+                }
+            } catch (\Exception $e) {
+                // The API encountered a problem before the script started executing.
+                \Log::error('Caught exception: '.$e->getMessage()."\n");
+            }
+
+            $maxRetries--;
         }
+
+        return [
+            'code' => 500,
+            'error' => 'Fatal error! Please contact NxSpace Admin!',
+        ];
     }
 
     /**
