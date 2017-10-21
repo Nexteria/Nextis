da9e9ae549f741446909128ee05866957e3ffe8a
Michal Porubsky <mikx@ksp.sk>
2017-10-21 15:33:04 +0200

    Pridana ikonka

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Nexteria Space</title>
+        <link rel="shortcut icon" href="{{{ asset('img/favicon.ico') }}}">
         {!! Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css') !!}
         {!! Html::style('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css') !!}
         {!! Html::style('https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css') !!}
